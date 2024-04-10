98347e0badd56cde7bf97e4b1c21dda93ed5eb35
Deven Mallamo <neved1234@gmail.com>
2024-04-09 20:44:22 -0400

    removed bloat

--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,16 @@
     #include "omp.h"
 #endif
 
-int num_frames = 24 * 30; //24 frames per second to create 30 second video
 int iterations = 10000; //This is the number of iterations we will use to calculate the veloci   ty of the mandelbrot set
 int width = 1000;
 int height = 1000;
 
-// According to https://en.wikipedia.org/wiki/Misiurewicz_point, this is a good point to zoom in on. Will set it as this for default
-double zoom_point_real = -.77568377;
-double zoom_point_imaginary = .13646737;
-double zoom_factor = .001; //This is the amount we will zoom in by between each frame
-
 //These will change as we zoom in, real and imaginary max and min.
 //Keeping them as real numbers for now, since the imaginary conversion happens in in the loop.
 double r_min = -2;
-double r_max = 1;
+double r_max = 2;
 double i_min = -2;
-double i_max = 1;
+double i_max = 2;
 
 void serial() {
     printf("bruh serial mandelbrot generation\n");
@@ -91,14 +85,12 @@ void parallel(int num_of_threads) {
     printf("Done\n");
 }
 
-
-
 int main (int argc, char *argv[]) {
     // If the OMP flag is on, it will trigger the parallel version - otherwise it will do the serial version
     double start_time = omp_get_wtime();
 
     #ifdef OMP=ON
-        parallel(1);
+        parallel(24);
     #else
         serial();
     #endif
